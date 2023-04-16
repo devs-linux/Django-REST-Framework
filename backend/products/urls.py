139a23054bae098f0e66ccda1f70902f715006a9
Dev <devs.linux.email@gmail.com>
2023-04-16 20:21:10 +0200

    Added the mixin view

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,17 +1,32 @@
 from django.urls import path
 
-from .views import product_alt_view, product_detail_view, product_list_create_view, product_update_view, product_delete_view
+from .views import (
+    product_alt_view,
+    product_detail_view,
+    product_list_create_view,
+    product_update_view,
+    product_delete_view,
+    product_mixin_view,
+)
 
 # Class views
-urlpatterns = [
-    path("", product_list_create_view),
-    path("<int:pk>/", product_detail_view),
-    path("<int:pk>/update/", product_update_view),
-    path("<int:pk>/delete/", product_delete_view)
-]
+# urlpatterns = [
+#     path("", product_list_create_view),
+#     path("<int:pk>/", product_detail_view),
+#     path("<int:pk>/update/", product_update_view),
+#     path("<int:pk>/delete/", product_delete_view)
+# ]
 
 # Function views
 # urlpatterns = [
 #     path("", product_alt_view),
 #     path("<int:pk>/", product_alt_view),
 # ]
+
+# Mixin views
+urlpatterns = [
+    path("", product_mixin_view),
+    path("<int:pk>/", product_mixin_view),
+    path("<int:pk>/update/", product_mixin_view),
+    path("<int:pk>/delete/", product_mixin_view),
+]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
@@ -46,6 +46,44 @@ class ProductDestroyAPIView(generics.DestroyAPIView):
 
 
 product_delete_view = ProductDestroyAPIView.as_view()
+
+
+class ProductMixinView(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView,
+):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = "pk"
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        content = serializer.validated_data.get("content") or None
+        if content is None:
+            content = "No content provided"
+
+        serializer.save(content=content)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+product_mixin_view = ProductMixinView.as_view()
 
 
 @api_view(["GET", "POST"])
